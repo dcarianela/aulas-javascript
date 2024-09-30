28772d1ed9e2e7a13b9e0a79e85804be9da3f591
dcarianela <aki.hayakawa5577@gmail.com>
2024-09-30 11:55:35 -0300

    Log, Arrays enfim

---
+++ 03-arrays.js
@@ -0,0 +1,10 @@
+// Declarando um array
+let alunos = ["Sunoo", "Jungwon", "Ni-ki", "Jake"];
+
+// Exibindo a estrutura do array
+console.log(alunos);
+
+// Acessando um elemento específico do array
+console.log(`${alunos[0]} has my whole heart! 💗`);
+
+
